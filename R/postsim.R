a0b833c06e98cef73ca1543d7c94243a3d9bf558
Wim Delva <wimdelva@Wims-MacBook-Pro.local>
2016-06-22 23:14:22 +0200

    inserted pseudo-code for functions to compute and examine degree distribution

--- R/postsim.R
+++ R/postsim.R
@@ -26,7 +26,6 @@ readthedata <- function(modeloutput){
   readetable <- readcsvcolumns::read.csv.columns(eventlogfilename, has.header = FALSE, column.types = "rssiirsiir")
   etable <- data.table::setDT(readetable)
   etable.colnames <- colnames(etable)
-  #etable <- fread(eventlogfilename, sep = ",", skip = 0)
   if (ncol(etable) == 10){
     data.table::setnames(etable, etable.colnames,
              c("eventtime", "eventname", "p1name", "p1ID", "p1gender", "p1age", "p2name", "p2ID", "p2gender", "p2age"))
@@ -35,19 +34,43 @@ readthedata <- function(modeloutput){
              c("eventtime", "eventname", "p1name", "p1ID", "p1gender", "p1age", "p2name", "p2ID", "p2gender", "p2age", "extradescript", "value"))
 
   }
-  #etable <- setDT(read.csv.columns(eventlogfilename, has.header = FALSE, column.types = "rssiirsiir"))
-  #etable <- fread(eventlogfilename, sep = ",", skip = 0)
-  #Set(etable, c("col_001", "col_002", "col_003", "col_004", "col_005", "col_006", "col_007", "col_008", "col_009", "col_010"),
-  #         c("eventtime", "eventname", "p1name", "p1ID", "p1gender", "p1age", "p2name", "p2ID", "p2gender", "p2age"), allow.absent=T)
   ttable <- data.table::fread(treatmentlogfilename, sep  = ",", skip = 0)
-  inputparamtable <- data.table::fread(inputparamlogfilename, sep  = ",", skip = 0)
+  itable <- data.table::fread(inputparamlogfilename, sep  = ",", skip = 0)
 
   if (file.exists(periodiclogfilename)){
     ltable <- data.table::fread(periodiclogfilename, sep = ",", skip = 0)
-    outputtables <- list(ptable = ptable, rtable = rtable, etable = etable, ttable = ttable, inputparamtable, ltable = ltable)
+    outputtables <- list(ptable = ptable, rtable = rtable, etable = etable, ttable = ttable, itable = itable, ltable = ltable)
   } else {
-    outputtables <- list(ptable = ptable, rtable = rtable, etable = etable, ttable = ttable, inputparamtable)
+    outputtables <- list(ptable = ptable, rtable = rtable, etable = etable, ttable = ttable, itable = itable)
   }
   return(outputtables)
 
 }
+
+
+degree.df.maker <- function(datalist = datalist, survey.time = 10, window.width = 1, only.new = TRUE){
+  # For the people in the datalist,
+  # make a dataframe that has the variables of the ptable,
+  # but in addition, also the number of relationships that were ongoing (if only.new == FALSE)
+  # or that were newly formed (if only.new == TRUE) at some time in the survey window.
+  # (E.g. The one-year period from 9 to 10 years into the simulation).
+
+  return(degree.df)
+}
+
+degree.hist.maker <- function(dataframe = degree.df){
+  # Some plotting commands, e.g. using ggplot2,
+  # to visualise the degree distribution.
+}
+
+degree.distr.fitter <- function(dataframe = degree.df){
+  # Fit various parametric distribution to the degree data
+  # and evaluate / compare their goodness-of-fit.
+  # Normal, Poisson, Negative Bionomial, Gamma, Beta, Log-normal, Weibull, ...
+  # And return the parameters (e.g. mean and variance or scale and shape) of the
+  # best fitting distribution.
+}
+
+
+
+
