f5d472467d3068e4efa94c378ed0007e7e9d9b09
Wim Delva <wimdelva@winlanstias.sun.ac.za>
2016-07-12 15:12:13 +0200

    moving degree.df.maker.R to the R folder and deleting old placeholding files. Plus updating Testscript.Rmd

--- R/degree.df.maker.R
+++ R/degree.df.maker.R
@@ -1,9 +1,42 @@
-degree.df.maker <- function(datalist = datalist, survey.time = 10, window.width = 1, only.new = TRUE){
-  # For the people in the datalist,
-  # make a dataframe that has the variables of the ptable,
-  # but in addition, also the number of relationships that were ongoing (if only.new == FALSE)
-  # or that were newly formed (if only.new == TRUE) at some time in the survey window.
-  # (E.g. The one-year period from 9 to 10 years into the simulation).
+#' Degree distribution of recent sexual relationships.
+#'
+#'  \code{degree.df.maker()} creates a table of two columns, "ID" and "degree" from the df dataframe,
+#'  with specific degrees of new relationships and ongoing relationships.
+#'
+#'  The degree distribution shows the total number of relationships that
+#'  were ongoing (if only.new == FALSE) or that were newly formed (if only.new == TRUE)
+#'  at some time in the survey window.
+#'
+#'  @param df The dataframe that is produced by \code{\link{agemix.df.maker()}}
+#'  @param survey.time period of the survey or simulation.
+#'  @param window.width period before the end of the survey e.g 1 year before the end of the survey.
+#'  @param only.new  relationships that were newly started during window.width (i.e. the individual
+#'  was NEVER in a relationship with these partners before the start of the window).
+#'
+#'  @return A dataframe, degree.df that has the columns "ID", "degree".
+#'  @examples
+#'  load(dataframe)
+#'  degree.df <- degree.df.maker(df = dataframe, survey.time = 10, window.width = 1, only.new = TRUE)
+#'  @importFrom magrittr %>%
+#'  @import dplyr
 
+degree.df.maker <- function(df,
+                            survey.time,
+                            window.width,
+                            only.new = TRUE){
+
+# newly formed relationships "else" ongoing relationships.
+  {if(only.new)
+  {df<-filter(df, FormTime >=survey.time-window.width, DisTime >= survey.time)}
+    else
+    {df<-filter(df, FormTime < survey.time-window.width, DisTime >= survey.time)}
+  }
+# unique relid's(those in the relationship) of each ID.
+  uniqueOut<-df %>%
+    select(ID, relid) %>%
+    distinct %>%
+    rename(Degree= relid)
+# counting total number of relationships each ID has.
+  degree.df<-aggregate(Degree ~ ID, data = uniqueOut, length)
   return(degree.df)
 }
