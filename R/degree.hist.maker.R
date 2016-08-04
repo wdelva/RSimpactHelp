ad3e5ea11a2f888c3901f540af9515e445637f10
Wim Delva <wimdelva@Wims-MacBook-Pro.local>
2016-08-04 14:42:22 +0200

    Turning the placeholder into a first stab at an actual function.

--- R/degree.hist.maker.R
+++ R/degree.hist.maker.R
@@ -1,4 +1,22 @@
+#' Plot the histogram of a degree distribution.
+#'
+#' Plot the histogram of the degree distribution that is stored in degree.df$Degree, after running \code{\link{degree.df.maker()}}
+#'
+#' @param dataframe The dataframe that is produced by \code{\link{degree.df.maker()}}
+#' @return a ggplot2 object
+#' @examples
+#' degree.hist.maker <- degree.hist.maker(dataframe = degree.df)
+#' @importFrom magrittr %>%
+#' @import dplyr
+#' @import ggplot2
+
 degree.hist.maker <- function(dataframe = degree.df){
-  # Some plotting commands, e.g. using ggplot2,
-  # to visualise the degree distribution.
+  fig <- dataframe %>%
+    ggplot(aes(x=Degree)) +
+    geom_histogram(binwidth = 1, aes(y=..density..)) +
+    xlab("Degree") +
+    ylab("Density") +
+    #facet_wrap(~Gender, nrow=1)
+    theme_bw()
+  return(fig)
 }
