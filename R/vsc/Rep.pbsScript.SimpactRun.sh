04347fb23c17ab61ca58ba1b594329de44547f3e
Trust Chibawara <trust@aims.ac.za>
2016-11-03 10:13:34 +0200

    removed the comma in max.chunck

--- R/vsc/Rep.pbsScript.SimpactRun.sh
+++ R/vsc/Rep.pbsScript.SimpactRun.sh
@@ -3,7 +3,7 @@ B = 400
 while [ $B -le 8000 ]
 do
   echo $A
-  cat simpact.wrapper.easyABC.run.R | sed  -e  "s/main.filename <- .*/main.filename <- 'INPUT.df-10Points10Par2016-11-02.csv'/" -e  "s/min.chunk <- .*/min.chunk <- ${A}/" -e "s/max.chunk <- .*/max.chunk <- ${B},/" -e "s/sim_repeat <- .*/sim_repeat <- 16/" -e "s/ncluster <- .*/ncluster <- 5/" > simpact.wrapper.easyABC.run.${A}.R
+  cat simpact.wrapper.easyABC.run.R | sed  -e  "s/main.filename <- .*/main.filename <- 'INPUT.df-10Points10Par2016-11-02.csv'/" -e  "s/min.chunk <- .*/min.chunk <- ${A}/" -e "s/max.chunk <- .*/max.chunk <- ${B}/" -e "s/sim_repeat <- .*/sim_repeat <- 16/" -e "s/ncluster <- .*/ncluster <- 5/" > simpact.wrapper.easyABC.run.${A}.R
   
   cat testABCrejectSimpactRun.pbs | sed -e "s/simpact.wrapper.easyABC.run.8000/simpact.wrapper.easyABC.run.${A}/" -e "s/walltime=00:30:00/walltime=04:00:00/" -e "s/simpact.wrapper.easyABC.run.R/simpact.wrapper.easyABC.run.${A}.R/" > testABCrejectSimpactRun.${A}.pbs
   qsub testABCreject.AIMS.${A}.pbs
@@ -11,5 +11,3 @@ do
   A =`expr $B + 1`
   B =`expr $B + 400`
 done
-
-cd /user/data/gent/vsc400/vsc40070/simpact-test/code/
