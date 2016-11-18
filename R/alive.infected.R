4e0279095a1dcdf16bfccc36201fb8851f95053f
Trust Chibawara <trust@aims.ac.za>
2016-11-18 11:38:23 +0200

    add the site option

--- R/alive.infected.R
+++ R/alive.infected.R
@@ -1,16 +1,27 @@
-#' Subset the datalist ptable to people alive at a point in time and add HIV status.
-#'
 #' Subset the datalist ptable to include only people who were alive at a point in time and add their HIV status at that point in time.
 #'
-#' @param DT The person table (ptable) that is produced by \code{\link{readthedata()}}
+#' @param datalist The datalist that is produced by \code{\link{readthedata()}}
 #' @param timepoint Point in time at which the subset should be created and HIV status should be evaluated.
+#' @param site Select only the particular site from the study, if all ignore site/use all sites.
 #' @return a data.table that only includes people who were alive at the timepoint and that records their HIV status.
 #' @examples
-#' alive.twenty.dt <- alive.infected(DT = datalist$ptable, timepoint = 20)
+#' alive.twenty.dt <- alive.infected(DT = datalist, timepoint = 20)
 
-alive.infected <- function(DT = datalist$ptable,
-                           timepoint = 20){ # arguments are the personlog data.table and a point in time
-  DTalive <- subset(DT, TOB <= timepoint & TOD > timepoint)
-  DTalive$Infected <- timepoint >= DTalive$InfectTime # Now we allocate infection status to all people in our table of living people
+alive.infected <- function(datalist = datalist,
+                           timepoint = 40, site="All"){ # arguments are the personlog data.table and a point in time
+  DT <- datalist$ptable
+  if(site=="All"){
+    DTalive <- subset(DT, TOB <= timepoint & TOD > timepoint)
+  }else{
+    facilities.df <- read.cv(datalist$itable$facilities.geo.coords)
+    facilities.df <- filter(facilities.df, Facility = site[1])
+    DTalive <- subset(DT, TOB <= timepoint & TOD > timepoint
+                      & XCoord==facilities.df$Longitude
+                      & YCoord==facilities.df$Latitude)
+  }
+
+  #DTalive$Infected <- timepoint >= DTalive$InfectTime # Now we allocate infection status to all people in our table of living people
+
+  DTalive <- DTalive %>%  mutate(Infected = (timepoint >= InfectTime))
   return(DTalive)
 }
