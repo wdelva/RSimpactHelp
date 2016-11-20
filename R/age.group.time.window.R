2b97a27b35a0a63616dfe22a15da532bc399794c
Trust Chibawara <trust@aims.ac.za>
2016-11-21 00:11:34 +0200

    Fixed case1 upper bound on Keep if

--- R/age.group.time.window.R
+++ R/age.group.time.window.R
@@ -1,6 +1,6 @@
 #' Subset the datalist ptable to include only people who existed within the time window and of age
-#' group. 
-#' Add the 
+#' group.
+#' Add the
 #'
 #' @param datalist The datalist that is produced by \code{\link{readthedata()}}
 #' @param timewindow Point in time at which the subset should be created and HIV status should be evaluated.
@@ -12,8 +12,8 @@
 
 age.group.time.window <- function(datalist = datalist,
                                   agegroup = c(15,30),
-                                  timewindow = c(15,30),
-                                  site="All"){ 
+                                  timewindow = c(10,40),
+                                  site="All"){
   DT <- datalist$ptable
   if(site=="All"){
     DTexists.timewindow <- subset(DT, TOB < (timewindow[2]- agegroup[1]) & TOD > timewindow[1]) # you need to be at least agegroup[1] to be able to be in this df
@@ -28,33 +28,33 @@ age.group.time.window <- function(datalist = datalist,
   # Case1
   # TOB > tw- and TOD < tw+ keep [age- <= TOD - TB <= age+]
   case1 <- subset(DTexists.timewindow, TOB > timewindow[1] & TOD < timewindow[2])
-  case1 <- subset(case1, agegroup[1] <= (TOD-TOB) & (TOD-TOB) <= agegroup[2])
+  case1 <- subset(case1, agegroup[1] <= (TOD-TOB))
   case1 <- case1 %>% mutate(MinAgeTW1 = 0, MaxAgeTW2 = TOD - TOB)
-  
-  # Case2 
+
+  # Case2
   # TOB > tw- and alive (TOD > tw+ or TOD=Inf) keep [age- <= tw+ - TB <= age+]
   case2 <- subset(DTexists.timewindow, TOB > timewindow[1] & (TOD < timewindow[2] | TOD==Inf))
   case2 <- subset(case2, agegroup[1] <= (timewindow[2] - TOB) & (timewindow[2] - TOB) <= agegroup[2])
   case2 <- case2 %>% mutate(MinAgeTW1 = 0, MaxAgeTW2 = timewindow[2] - TOB)
-  
-  # Case3 
+
+  # Case3
   # TOB > 0 and TOB < tw- and TOD < tw+ keep [age- <= TOD - TB <= age+]
   case3 <- subset(DTexists.timewindow, TOB > 0 & TOB < timewindow[1] & TOD < timewindow[2])
   case3 <- subset(case3, agegroup[1] <= (TOD - TOB) & (TOD - TOB) <= agegroup[2])
   case3 <- case3 %>% mutate(MinAgeTW1 = timewindow[1]-TOB, MaxAgeTW2 = TOD - TOB)
-  
+
   # Case4
   # TOB > 0 and TOB < tw- and alive (TOD > tw+ or TOD=Inf) keep [age- <= tw+ - TB <= age+]
   case4 <- subset(DTexists.timewindow, TOB > 0 & TOB < timewindow[1] & (TOD > timewindow[2] | TOD ==Inf))
   case4 <- subset(case4, agegroup[1] <= (timewindow[2] - TOB) & (timewindow[2] - TOB) <= agegroup[2])
   case4 <- case4 %>% mutate(MinAgeTW1 = timewindow[1]-TOB, MaxAgeTW2 = timewindow[2] - TOB)
-  
+
   # Case5
   # TOB < 0 and TOD < tw+ keep [age- <= TOD - TB <= age+]
   case5 <- subset(DTexists.timewindow, TOB < 0 & TOD < timewindow[2])
   case5 <- subset(case5, (timewindow[1] - TOB) <= agegroup[2])
   case5 <- case5 %>% mutate(MinAgeTW1 = timewindow[1]-TOB, MaxAgeTW2 = TOD - TOB)
-  
+
   # Case6
   # TOB < 0 and alive (TOD > tw+ or TOD=Inf) keep [age- <= tw+ - TB <= age+]
   case6 <- subset(DTexists.timewindow, TOB < 0 & (TOD > timewindow[2] | TOD ==Inf))
@@ -62,7 +62,7 @@ age.group.time.window <- function(datalist = datalist,
   case6 <- case6 %>% mutate(MinAgeTW1 = timewindow[1]-TOB, MaxAgeTW2 = timewindow[2] - TOB)
 
   DTexists.timewindow <- rbind(case1, case2, case3, case4, case5, case6)
-  
+
   #check here (You should get TRUE)
   #(nrow(DTexists.timewindow) == length(unique(DTexists.timewindow$ID)))
   #Else dplyr::intersect(case1$ID, case5$ID) #check which case is a problem
