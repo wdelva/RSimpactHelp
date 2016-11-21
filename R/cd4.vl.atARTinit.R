0657b266ec08295b9a6147e1fd6339b09f40fa61
Chibawara <tchibawara@sun.ac.za>
2016-11-22 00:31:45 +0200

    not relevant

--- R/cd4.vl.atARTinit.R
+++ R/cd4.vl.atARTinit.R
@@ -17,25 +17,25 @@ cd4.vl.atARTinit <- function(datalist = datalist, agegroup = c(15,30),
   gender.id <-1
   if(gender!="Male"){gender.id = 0}
 
-  cd4.vl.atARTinit <- age.group.time.window(datalist = datalist, 
+  cd4.vl.atARTinit <- age.group.time.window(datalist = datalist,
                                                    agegroup = agegroup, timewindow = timewindow, site="All")
 
-  cd4.vl.atARTinit <- subset(cd4.vl.atARTinit, TreatTime !=Inf & Gender == gender.id) #HIV negative individuals
-  
+  cd4.vl.atARTinit <- subset(cd4.vl.atARTinit, TreatTime !=Inf & Gender == gender.id) #HIV positive Gender individuals
+
   raw.df <- data.frame(cd4.vl.atARTinit)
   art.df <- subset(datalist$ttable, ID %in% cd4.vl.atARTinit$ID & TStart > timewindow[1] & TStart < timewindow[2])
-  
+
   # Now we apply the left_join dplyr function to add the ART status to raw.df.
   raw.df <- left_join(x = raw.df, y = art.df, by = c("ID", "Gender"))
-  
-  #select those individuals who started their treatment when their CD4 count what between the given threshold 
+
+  #select those individuals who started their treatment when their CD4 count what between the given threshold
   cd4count.atARTInit <- subset(raw.df, cd4count[1] <= CD4atARTstart &  CD4atARTstart <= cd4count[2])
-  
+
   cl.atARTinit <- subset(raw.df, viralload[1] <= log10SPVL &  log10SPVL <= viralload[2])
-  
+
   cd4count.atARTInit <- nrow(cd4count.atARTInit)
   vl.atARTinit <- nrow(vl.atARTinit)
-  
+
   cd4.vl.atARTinit <- c(cd4count.atARTInit, vl.atARTinit)
 
   return(cd4.vl.atARTinit)
