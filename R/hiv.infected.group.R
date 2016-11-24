c5bdf6b60b3592fb47349ba05c1ce1e23d0cb820
Chibawara <tchibawara@sun.ac.za>
2016-11-24 22:53:58 +0200

    fixed parameter definations and results has both gender

--- R/hiv.infected.group.R
+++ R/hiv.infected.group.R
@@ -1,31 +1,30 @@
 #' Calculate the number of HIV infections that occured between two time points.
 #'
 #' @param datalist The datalist that is produced by \code{\link{readthedata()}}
-#' @param timewindow HIV transmission that took place between this simulation time e.g timewindow = c(20,30)
-#' @param gender HIV infected by this gender.
+#' @param timewindow HIV transmission that took place between this simulation time e.g timewindow = c(20, 30)
 #' @param agegroup HIV infection within this agegroup.
 #' @param site Select only the particular site from the study, if all ignore site/use all sites.
-#' @return a list with the transmission network data, as required by the epi2tree function.
+#' @return data.frame with number of infection aggregated by Gender
 #' @examples
-#' hiv.infected <- hiv.infected.group(datalist = datalist, agegroup=c(15,30), timewindow=c(20,40), gender="Male", site="All")
+#' hiv.infected <- hiv.infected.group(datalist = datalist, agegroup=c(15, 30), timewindow=c(20, 40), gender="Male", site="All")
+#'
+#' @import dplyr
 
-hiv.infected.group <- function(datalist = datalist,
-                                agegroup = c(15,30),
-                                timewindow = c(20,30),
-                                gender = "Male", site="All"){
-  gender.id <-1
-  if(gender!="Male"){gender.id = 0}
-  #person.alive.infected <- alive.infected(datalist, timewindow, site="All") #Remove HIV negative individuals till upper of timewindow
+hiv.infected.group <- function(datalist = datalist, agegroup = c(15, 30), timewindow = c(20, 30), site="All"){
 
   person.alive.infected <- age.group.time.window(datalist = datalist,
-                                                 agegroup = agegroup, timewindow = timewindow,site="All")
+                                                 agegroup = agegroup, timewindow = timewindow, site="All")
 
   person.alive.infected <- subset(person.alive.infected, InfectTime!=Inf)
 
-  person.alive.infected <- subset(person.alive.infected, InfectTime>exposure.start &
-                                    InfectTime<=exposure.end & Gender== gender.id) #include TOD but this should be excluded already
-
-  hiv.infect.count <- nrow(person.alive.infected)
+  #exlude TOD but this should be excluded already
+  person.alive.infected <- subset(person.alive.infected, InfectTime>exposure.start & InfectTime<=exposure.end)
+  #aggregate by gender
+  hiv.infect.count <- data.frame(dplyr::summarise(dplyr::group_by(person.alive.infected, Gender),
+                                                  InfectedCount = n()))
+  #indicate the gender stats
+  hiv.infect.count$Gender[hiv.infect.count$Gender==0] <- "Woman"
+  hiv.infect.count$Gender[hiv.infect.count$Gender==1] <- "Man"
 
   return(hiv.infect.count)
 }
