0aa9425aabf00f7be8ed7d7b6d57837698629fb9
Chibawara <tchibawara@sun.ac.za>
2016-11-24 22:51:46 +0200

    fixed parameter definations and the results includes both gender

--- R/VL.atARTinit.R
+++ R/VL.atARTinit.R
@@ -1,33 +1,37 @@
 #' A function that returns the total number of people between two points in simulation
-#' time for a particular age group and gender whose VL and CD4count are as indicated
+#' time for a particular age group whose VL is within a threshold
 #'
 #' @param datalist The datalist that is produced by \code{\link{readthedata()}}
 #' @param timewindow alive people within this simulation time e.g timewindow = 30.
-#' @param gender alive gender.
 #' @param agegroup alive people within this agegroup.
 #' @param viralload log10 set point viral load threshold at the time of ART initiation.
 #' @param site Select only the particular site from the study, if all ignore site/use all sites.
 #' @return the total number of of people whose VL at ART initiation are within a given threshold
 #' @examples
-#' vl.atARTinit <- vl.atARTinit(datalist = datalist, agegroup=c(15,30), timewindow=c(15,30), viralload=c(3,4), gender="Male", site="All")
+#' vl.atARTinit <- vl.atARTinit(datalist = datalist, agegroup = c(15, 30), timewindow = c(15, 30), viralload = c(3, 4), site="All")
+#'
+#' @importFrom magrittr %>%
+#' @import dplyr
 
-vl.atARTinit <- function(datalist = datalist, agegroup = c(15,30),
-                             timewindow = c(15,30), viralload=c(3,4), gender = "Male", site="All"){
-  gender.id <-1
-  if(gender!="Male"){gender.id = 0}
+vl.atARTinit <- function(datalist = datalist, agegroup = c(15, 30),
+                             timewindow = c(15, 30), viralload = c(3, 4), site="All"){
 
-  vl.atARTinit <- age.group.time.window(datalist = datalist,
+  vl.atARTinit.Count <- age.group.time.window(datalist = datalist,
                                                    agegroup = agegroup, timewindow = timewindow, site="All")
 
-  vl.atARTinit <- subset(vl.atARTinit, TreatTime !=Inf & Gender == gender.id) #HIV positive Gender individuals
+  vl.atARTinit.Count <- subset(vl.atARTinit.Count, TreatTime !=Inf) #HIV positive individuals
 
-  vl.atARTinit <- subset(raw.df, viralload[1] <= log10SPVL &  log10SPVL <= viralload[2])
+  vl.atARTinit.Count <- vl.atARTinit.Count %>% mutate(VLThresholdatARTinit = (viralload[1] <= log10SPVL &  log10SPVL <= viralload[2]))
 
-  # Should we correct for dropout and restart [Simpact simply uses the Set-Point Viral Load]
   # log10SPVL is not affected by treatment
+  #provide a summary of those that are on treatmen and those that started below a threshold
+  vl.atARTinit.Count <- data.frame(dplyr::summarise(dplyr::group_by(vl.atARTinit.Count, Gender),
+                                                    TotalCases = n(),
+                                                    VLatARTinitThreshold =sum(VLThresholdatARTinit)))
 
-  vl.atARTinit <- nrow(vl.atARTinit)
+  vl.atARTinit.Count$Gender[vl.atARTinit.Count$Gender==0] <- "Woman"
+  vl.atARTinit.Count$Gender[vl.atARTinit.Count$Gender==1] <- "Man"
 
-  return(vl.atARTinit)
+  return(vl.atARTinit.Count)
 }
 
