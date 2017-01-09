2cd405e91f9b190a2d2bb653d93ea200cce386eb
Trust Chibawara <trust@aims.ac.za>
2017-01-09 12:22:12 +0200

    useful functions reference

---
+++ R/vsc/usefulfunctions.R
@@ -0,0 +1,7 @@
+#attempt to normalise test_mean <- (x-xbar)/sqrt(sum((x-xbar)^2)) 
+#x11 <- inputANDoutput.select$growth.rate - mean(inputANDoutput.select$growth.rate, na = TRUE)
+# x12 <- (inputANDoutput.select$growth.rate - mean(inputANDoutput.select$growth.rate, na = TRUE))^2
+# x13 <- sum(x12, na=TRUE)
+# x14 <-sqrt(x13)
+# test_mean <- (x11)/ x14
+# multi.hist(test_mean)
