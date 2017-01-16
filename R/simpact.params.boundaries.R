e1248eb9989065ea92d28e05f46d0c323570dd0f
Trust Chibawara <trust@aims.ac.za>
2017-01-16 11:06:27 +0200

    Function to return the simpact boundaries, defaulted to over 14 as a start

---
+++ R/simpact.params.boundaries.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env/ Rscript
+#' These are the preset boundaries for most of the parameters that are likely to be calibrated in simpact.
+#' Most of these parameters will be standard in \code{\link{simpact.config.inputs()}} stage.
+#' The user can set the simulation specific parameters as needed
+#'
+#' @param default is to return all the parameters used during the \code{\link{simpact.config.inputs()}} setup.
+#' @return a list of boundaries of parameters to be calibrated
+#' @examples
+#' varied.params.bound <- simpact.params.boundaries()[[1]]
+
+simpact.params.boundaries <- function(...){
+
+  arg.list <- list(person.eagerness.man.dist.gamma.a = c(0.1, 2),
+                   person.eagerness.man.dist.gamma.b = c(5, 60),
+                   person.eagerness.woman.dist.gamma.a = c(0.1, 2),
+                   person.eagerness.wooman.dist.gamma.b = c(5, 60),
+                   conception.alpha_base = c(-3.6, -1.2),
+                   formation.hazard.agegapry.numrel_man = c(-1.5, -0.1),
+                   formation.hazard.agegapry.eagerness_diff = c(-0.1, 0),
+                   formation.hazard.agegapry.gap_factor_man_exp = c(-1.5, -0.4),
+                   person.agegap.man.dist.normal.mu = c(0, 4),
+                   person.agegap.woman.dist.normal.mu = c(0, 4),
+                   person.agegap.man.dist.normal.sigma = c(0.5, 2),
+                   person.agegap.woman.dist.normal.sigma = c(0.5, 2),
+                   formation.hazard.agegapry.numrel_woman = c(-1.5, -0.1),
+                   formation.hazard.agegapry.gap_factor_woman_exp = c(-1.5, -0.4),
+                   formation.hazard.agegapry.baseline = c(1.5, 3),
+                   ...)
+
+  return(arg.list)
+}
+
