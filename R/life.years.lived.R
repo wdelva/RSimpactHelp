0574da8a2057c79cc0632e754a66f34a18c8f065
Trust Chibawara <trust@aims.ac.za>
2017-03-24 11:00:22 +0200

    Cleaned up the description and corrected the code flow and calculations.

--- R/life.years.lived.R
+++ R/life.years.lived.R
@@ -1,24 +1,32 @@
 #' A function that returns the total number of life-years lived between two points in simulation
 #' time for a particular age group and gender
 #'
-#' @param datalist The datalist that is produced by \code{\link{readthedata()}}
-#' @param timewindow alive people within this simulation time e.g timewindow = c(15, 30).
+#' @param datalist The datalist that is produced by \code{\link{readthedata}}
 #' @param agegroup alive people within this agegroup.
+#' @param timewindow alive people within this simulation time e.g timewindow = c(15, 30).
 #' @param site Select only the particular site from the study, if all ignore site/use all sites.
 #' @return the total number of life-years lived aggregated by gender.
 #' @examples
-#' life.years.lived <- life.years.lived(datalist = datalist, agegroup=c(15, 30), timewindow=c(15, 30), site="All")
+#' data(datalist)
+#' life.years.lived <- life.years.lived(datalist = datalist, agegroup=c(15, 30),
+#' timewindow=c(15, 30), site="All")
+#' life.years.lived
 #'
 #' @importFrom magrittr %>%
 #' @import dplyr
+#' @export
 
-life.years.lived <- function(datalist = datalist, agegroup = c(15, 30), timewindow = c(15, 30), site="All"){
+life.years.lived <- function(datalist = datalist,
+                             agegroup = c(15, 30),
+                             timewindow = c(15, 30), site="All"){
 
   person.alive.timewindow <- age.group.time.window(datalist = datalist,
-                                                   agegroup = agegroup, timewindow = timewindow, site="All")
+                                                   agegroup = agegroup,
+                                                   timewindow = timewindow, site="All")
 
 
-  person.alive.timewindow <- person.alive.timewindow %>% mutate(exposure.time.Alive = exposure.end - exposure.start)
+  person.alive.timewindow <- person.alive.timewindow %>%
+    dplyr::mutate(exposure.time.Alive = exposure.end - exposure.start)
   person.alive.timewindow <- subset(person.alive.timewindow, exposure.time.Alive > 0)
 
   ## Sum all people's contibution years
