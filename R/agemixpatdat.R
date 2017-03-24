cceedd5a1bed3a9c26e88d81e394012bc533c37c
Trust Chibawara <trust@aims.ac.za>
2017-03-24 10:57:33 +0200

    Cleaned up the description and corrected the code flow and calculations.

--- R/agemixpatdat.R
+++ R/agemixpatdat.R
@@ -1,7 +1,7 @@
 #' Age-mixing pattern model data.
 #'
 #' This is an example datalist that could be created from the function
-#' \code{\link{pattern.modeller()}}. It contains two elements. The first is a
+#' pattern.modeller. It contains two elements. The first is a
 #' subsetted dataframe of relationships from 15-30 year old men and women that
 #' were ongoing during the year prior to year 30 of the simulation. This
 #' dataframe includes population-average partner age predictions from a linear
@@ -9,10 +9,14 @@
 #' age-mixing pattern outputs from the model. These outputs can be found in the
 #' second dataframe.
 #'
+#' @docType data
+#'
+#' @usage data(agemixpatdat)
+#'
 #' @format The first dataframe in the list has 16579 rows and 26 variables. The
 #'   second dataframe has 1 row and 14 variables. More detailed descriptions of
 #'   the contents of the dataframes can be found in
-#'   \code{\link{pattern.modeller()}}
+#'   \code{\link{pattern.modeller}}
 
 "agemixpatdat"
 
