b978cbe1df0e273a9f3c8fc250187bca62eaba36
Trust Chibawara <trust@aims.ac.za>
2017-03-24 10:57:39 +0200

    Cleaned up the description and corrected the code flow and calculations.

--- R/alive.infected.R
+++ R/alive.infected.R
@@ -1,14 +1,20 @@
-#' Subset the datalist ptable to include only people who were alive at a point in time and add their HIV status at that point in time.
+#' Subset the datalist ptable to include only people who were alive at a point
+#'  in time and add their HIV status at that point in time.
 #'
-#' @param datalist The datalist that is produced by \code{\link{readthedata()}}
-#' @param timepoint Point in time at which the subset should be created and HIV status should be evaluated.
+#' @param datalist The datalist that is produced by \code{\link{readthedata}}
+#' @param timepoint Point in time at which the subset should be created and HIV status
+#' should be evaluated.
 #' @param site Select only the particular site from the study, if all ignore site/use all sites.
-#' @return a data.table that only includes people who were alive at the timepoint and that records their HIV status.
+#' @return a data.table that only includes people who were alive at the timepoint and that
+#' records their HIV status.
 #' @examples
-#' alive.twenty.dt <- alive.infected(datalist = datalist.test, timepoint = 20, site = "Siphocosini Clinic")
+#' data(datalist)
+#' alive.at.twenty.dt <- alive.infected(datalist = datalist, timepoint = 20,
+#' site = "All")
 #'
 #' @importFrom magrittr %>%
 #' @import dplyr
+#' @export
 
 alive.infected <- function(datalist = datalist,
                            timepoint = 40,
@@ -25,16 +31,17 @@ alive.infected <- function(datalist = datalist,
     colnames(facilities.df) <- c("facility.xy", "XCoord", "YCoord")
 
     for (i in 1:nrow(DT)) {
-      fc.id <-
-        which.min(sqrt((DT[i, XCoord] - facilities.df$XCoord)^2 + (DT[i, YCoord] - facilities.df$YCoord)^2
-        ))
+      fc.id <- which.min(sqrt((DT[i, XCoord] - facilities.df$XCoord)^2 +
+                         (DT[i, YCoord] - facilities.df$YCoord)^2 ))
+
       DT[i, pf.index] <- facilities.df[fc.id, facility.xy]
     }
 
     DTalive <- subset(DT, TOB <= timepoint & TOD > timepoint & pfacility == site)
   }
 
-  # Now we allocate infection status to all people in our table of living people
-  DTalive <-  DTalive %>%  mutate(Infected = (timepoint >= InfectTime))
+  # Now we allocate infection status to all alive people in our table
+  DTalive <-  DTalive %>%  dplyr::mutate(Infected = (timepoint >= InfectTime))
+
   return(DTalive)
 }
