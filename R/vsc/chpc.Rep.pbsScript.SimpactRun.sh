867245b924a67c6397e096f8c834d473cd29df8e
Trust Chibawara <trust@aims.ac.za>
2017-04-03 18:09:52 +0200

    Update the script to edit new lines

--- R/vsc/chpc.Rep.pbsScript.SimpactRun.sh
+++ R/vsc/chpc.Rep.pbsScript.SimpactRun.sh
@@ -1,16 +1,16 @@
-A = 1
-B = 50
-while [ $B -le 1000 ]
+A = 6001
+B = 8000
+while [ $B -le 22000 ]
 do
   echo $A
-  cat simpact.wrapper.easyABC.run.R | sed -e "s/comp <- .*/comp <- 'chpc'/" -e  "s/min.chunk <- .*/min.chunk <- ${A}/" -e "s/max.chunk <- .*/max.chunk <- ${B}/" -e "s/ncluster.use <- .*/ncluster.use <- 16/" > simpact.wrapper.easyABC.run.${A}.R
+  cat simpact.wrapper.easyABC.run.4001.R | sed -e "s/min.chunk <- 4001/min.chunk <- ${A}/" -e "s/max.chunk <- 6000/max.chunk <- ${B}/" > simpact.wrapper.easyABC.run.${A}.R
 
-  cat chpc.testABCreject.SimpactRun.pbs | sed -e "s/simpact.wrapper.easyABC.run.100/simpact.wrapper.easyABC.run.${A}/" -e "s/walltime=00:30:00/walltime=04:00:00/" -e "s/simpact.wrapper.easyABC.run.R/simpact.wrapper.easyABC.run.${A}.R/" > testABCreject.SimpactRun.${A}.pbs
+  cat chpcSimpactRunTest.pbs | sed -e "s/simpact.wrapper.easyABC.run.4001/simpact.wrapper.easyABC.run.${A}/" -e "s/simpact.wrapper.easyABC.run.4001.R/simpact.wrapper.easyABC.run.${A}.R/" > chpcSimpactRunTest.${A}.pbs
 
-  qsub testABCreject.SimpactRun.${A}.pbs
+  qsub chpcSimpactRunTest.${A}.pbs
 
   sleep 4
 
   A = `expr $B + 1`
-  B = `expr $B + 50`
+  B = `expr $B + 2000`
 done
