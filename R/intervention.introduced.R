1bd2a7775ab05a39165ff7eb4bd1c6c0e2fd7328
Trust Chibawara <trust@aims.ac.za>
2017-04-27 21:33:25 +0200

    Added the piecewise cd4s during change of monitoring for treatment

--- R/intervention.introduced.R
+++ R/intervention.introduced.R
@@ -22,36 +22,31 @@ intervention.introduced <- function(simulation.type = "simpact-cyan"){
     art.intro["time"] <- 27
     art.intro["diagnosis.baseline"] <- 0 # Reset to 0, from its original value @ sim start
     art.intro["monitoring.cd4.threshold"] <- 100
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "100,250"
     art.intro["diagnosis.genderfactor"] <- 2
 
     # Gradual increase in CD4 threshold. in 2007:200. in 2010:350. in 2013:500
-
     art.intro2 <- list()
     art.intro2["time"] <- 30
     art.intro2["monitoring.cd4.threshold"] <- 200
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "200,350"
     art.intro2["diagnosis.genderfactor"] <- 1.5
 
     art.intro3 <- list()
     art.intro3["time"] <- 33
     art.intro3["monitoring.cd4.threshold"] <- 350
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "350,500"
     art.intro3["diagnosis.genderfactor"] <- 1
 
     art.intro4 <- list()
     art.intro4["time"] <- 36
     art.intro4["monitoring.cd4.threshold"] <- 500
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "500,650"
     art.intro4["diagnosis.genderfactor"] <- 0.5
-
-    hivseed.int1 <- list()
-    hivseed.int1["time"] <- 15
-    hivseed.int1["hivseed.amount"] <- 100
-
-    hivseed.int2 <- list()
-    hivseed.int2["time"] <- 35
-    hivseed.int2["hivseed.amount"] <- 100
 
     # person.art.accept.threshold.dist.fixed.value
 
-    iv <- list(art.intro, art.intro2, art.intro3, art.intro4, hivseed.int1, hivseed.int2)
+    iv <- list(art.intro, art.intro2, art.intro3, art.intro4)
 
   }else if(simulation.type == "maxart"){
     ### Eligibility increased from <200 to <350 in 2010 and then to <500 in 2015
@@ -62,35 +57,29 @@ intervention.introduced <- function(simulation.type = "simpact-cyan"){
     art.intro["time"] <- 27
     art.intro["diagnosis.baseline"] <- 0 # Reset to 0, from its original @ sim start
     art.intro["monitoring.cd4.threshold.prestudy"] <- 200
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "100,250"
     art.intro["diagnosis.genderfactor"] <- 2
 
     # Gradual increase in CD4 threshold. in 2007:200. in 2010:350. in 2014:500
-
     art.intro2 <- list()
     art.intro2["time"] <- 30
     art.intro2["monitoring.cd4.threshold.prestudy"] <- 200
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "200,350"
     art.intro2["diagnosis.genderfactor"] <- 1.5
 
     art.intro3 <- list()
     art.intro3["time"] <- 33
     art.intro3["monitoring.cd4.threshold.prestudy"] <- 350
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "350,500"
     art.intro3["diagnosis.genderfactor"] <- 1
 
     art.intro4 <- list()
     art.intro4["time"] <- 38
     art.intro4["monitoring.cd4.threshold.prestudy"] <- 500
+    art.intro["monitoring.interval.piecewise.cd4s"] <- "500,650"
     art.intro4["diagnosis.genderfactor"] <- 0.5
 
-    # person.art.accept.threshold.dist.fixed.value
-    hivseed.int1 <- list()
-    hivseed.int1["time"] <- 15
-    hivseed.int1["hivseed.amount"] <- 100
-
-    hivseed.int2 <- list()
-    hivseed.int2["time"] <- 35
-    hivseed.int2["hivseed.amount"] <- 100
-
-    iv <- list(art.intro, art.intro2, art.intro3, art.intro4, hivseed.int1, hivseed.int2)
+    iv <- list(art.intro, art.intro2, art.intro3, art.intro4)
   }else{
     iv <-list()
   }
