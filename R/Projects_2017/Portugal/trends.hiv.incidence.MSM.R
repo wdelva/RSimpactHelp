107ed7d2bd5d9f7ae37aabc202dda7ec207b80bd
David Niyukuri <niyukuri@aims.ac.za>
2017-06-03 16:41:09 +0200

    roadmpa of the calibration related to Portugal presentation

--- R/Projects_2017/Portugal/trends.hiv.incidence.MSM.R
+++ R/Projects_2017/Portugal/trends.hiv.incidence.MSM.R
@@ -1,19 +1,26 @@
 # Calibration of ABM with phylogenetic tree
 
-# 1. Run a master model with well deined parameter settings
+# 1. Run a master model with well defined parameter settings
 
 # sexual/transmission network + evolution of the virus across the transmission network
 #
 
-# 2. Calibrate one of the default model with epidmeiological statistics
+# 2. Calibrate the default model with epidmeiological statistics: 2 case
 
+# Ex.1
 # input parameters: numrel,diff and eagerness,diff
 # target statistics: average relationships for both partners and
-# standard deviation  relationships for both partners,
+# standard deviation  relationships for both partners
 
-# 3. Calibrate two of the default model with epidmeiological and phylogenetic statistics
+# Ex.2 : Chosen
+# input parameters: numrel,diff and eagerness,diff
+# target statistics: average relationships for men and women,
+# standard deviation  relationships for men and women, and average population growth rate
 
-# Ex.1 phylogentic statistic: slope of the fit line between size of sub-trees and the number of sub-trees
+
+# 3. Calibrate the default model with epidmeiological and phylogenetic statistics: 2 cases
+
+# Ex.1 phylogentic statistics: slope (Alpha) of the fit line between size of sub-trees and the number of sub-trees
 # input parameters: numrel,diff and eagerness,diff
 # target statistics: average relationships for both partners,
 # standard deviation  relationships for both partners, and
@@ -28,7 +35,7 @@
 # slope of the fit line between size of sub-trees and the number of sub-trees [target stat from a phylogeny]
 
 
-# Ex.2 phylogentic statistic: slope of the fit line between size of sub-trees and the number of sub-trees
+# Ex.3 phylogentic statistic: slope of the fit line between size of sub-trees and the number of sub-trees
 # input parameters: numrel,diff and eagerness,diff
 # target statistics: average relationships for both partners,
 # standard deviation  relationships for both partners, and
