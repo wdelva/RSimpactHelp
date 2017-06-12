9e83ab1f0f3cb4cb618b43774b49912d71ae7d0f
David Niyukuri <niyukuri@aims.ac.za>
2017-06-12 19:53:17 +0200

    add function to build transmssion tree (turn the transmission network in a phylogenetic object)

---
+++ R/trans.network2tree.R
@@ -0,0 +1,22 @@
+#' Build a transmission tree
+#'
+#' From the output of the \code{\link{transmNetworkBuilder.baseline()}} or \code{\link{transmNetworkBuilder.diff()}},
+#' we can build the tranmission tree
+#' @param transnetwork The phylo object produced by \code{\link{transmNetworkBuilder.baseline}} or \code{\link{transmNetworkBuilder.diff()}}.
+#' @return Transmission tree object (a phylo object)
+#' @examples
+#'
+#' transm.ls <- transmNetworkBuilder.baseline(datalist = datalist, endpoint = 40)
+#' transnetwork <- transm.ls[[16]]
+#' tree0 <- trans.network2tree(transnetwork) # transmission tree of seeds 16
+#'
+#' @importFrom expoTree epi2tree
+#'
+
+trans.network2tree <- function(transnetwork = transnetwork){
+
+  transm.phylo <- epi2tree(transnetwork)
+
+  return(transm.tree)
+
+}
