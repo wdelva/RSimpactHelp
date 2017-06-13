8f90b8f5857b473a98c43c9e88353e199f194822
David Niyukuri <niyukuri@aims.ac.za>
2017-06-13 22:52:09 +0200

    add option to split the time window in sml time intervals

--- R/transmission.rate.calculator.R
+++ R/transmission.rate.calculator.R
@@ -5,23 +5,61 @@
 #' @param datalist The datalist that is produced by \code{\link{readthedata}}
 #' @param timewindow Boundaries of the time window (lower bound < time <= upper bound)
 #' that should be retained, e.g. c(20, 40)
-#'
+#' @param int Logic parameter which is FALSE by deafault and means tha only one value for the transmission rate
+#' needed, it is TRUE when the time window is being split in small time interval and get different
+#' transmission rates within these time intervals
+#' @param by Time step unit to subdivide the time window
 #' data(datalist)
-#' transm.rate <- transmission.rate.calculator(datalist = datalist, timewindow = c(20, 40))
+#' transm.rate <- transmission.rate.calculator(datalist = datalist,
+#' timewindow = c(20, 40), int = FALSE, by=1)
 #' transm.rate
 #'
 #' @import dplyr
 
 transmission.rate.calculator <- function(datalist = datalist,
-                              timewindow = c(20, 40)){
+                              timewindow = c(20, 40), int = FALSE, by=1){
 
-  Infec.pop.table <- datalist$ptable[InfectType==1]
+  if(int==FALSE){
 
-  numb.infec.pop <- nrow(Infec.pop.table %>%
-                           subset(InfectTime <=timewindow[2] & InfectTime >= timewindow[1]))
+    Infec.pop.table <- datalist$ptable[InfectType==1]
+
+    numb.infec.pop <- nrow(Infec.pop.table %>%
+                             subset(InfectTime <=timewindow[2] & InfectTime >= timewindow[1]))
 
 
-  transm.rate <- numb.infec.pop / diff(timewindow)
+    transm.rate <- numb.infec.pop / diff(timewindow)
 
-  return(transm.rate)
+    return(transm.rate)
+  }
+
+  if(int==TRUE){
+
+    Infec.pop.table <- datalist$ptable[InfectType==1]
+
+    upper.limit <- ceiling(diff(timewindow)/by)
+
+    interv.time <- round(seq.int(timewindow[1], timewindow[2], length.out = upper.limit), digits = 0)
+
+    infec.pop.int <- vector()
+    trans.rate.int <- vector()
+
+
+    for(i in 0:(upper.limit-2)){
+
+      timewindow.int <- c(interv.time[1+i], interv.time[2+i])
+
+      infec.pop.numb <- nrow(Infec.pop.table %>%
+                               subset(InfectTime <=timewindow.int[2] & InfectTime >= timewindow.int[1]))
+
+
+      infec.pop.int <- c(infec.pop.int,  infec.pop.numb)
+
+
+      trans.rate.int <- c(trans.rate.int, (infec.pop.numb / diff(timewindow.int)))
+
+    }
+
+    return(trans.rate.int)
+  }
+
 }
