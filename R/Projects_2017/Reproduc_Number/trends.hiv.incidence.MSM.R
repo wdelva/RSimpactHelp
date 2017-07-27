636458c387ded6cd44e6549aa49327d7f0cbd09d
David Niyukuri <niyukuri@aims.ac.za>
2017-07-27 10:24:44 +0200

    rename folder fr Portugal to Reproduc_Number

---
+++ R/Projects_2017/Reproduc_Number/trends.hiv.incidence.MSM.R
@@ -0,0 +1,45 @@
+# Calibration of ABM with phylogenetic tree
+
+# 1. Run a master model with well defined parameter settings
+
+# sexual/transmission network + evolution of the virus across the transmission network
+#
+
+# 2. Calibrate the default model with epidmeiological statistics: 2 case
+
+# Ex.1
+# input parameters: numrel,diff and eagerness,diff
+# target statistics: average relationships for both partners and
+# standard deviation  relationships for both partners
+
+# Ex.2 : Chosen
+# input parameters: numrel,diff and eagerness,diff
+# target statistics: average relationships for men and women,
+# standard deviation  relationships for men and women, and average population growth rate
+
+
+# 3. Calibrate the default model with epidmeiological and phylogenetic statistics: 2 cases
+
+# Ex.1 phylogentic statistics: slope (Alpha) of the fit line between size of sub-trees and the number of sub-trees
+# input parameters: numrel,diff and eagerness,diff
+# target statistics: average relationships for both partners,
+# standard deviation  relationships for both partners, and
+# slope of the fit line between size of sub-trees and the number of sub-trees [target stat from a phylogeny]
+
+
+# Ex.2 phylogentic statistic: slope of the fit line between size of sub-trees and the number of sub-trees, and
+
+# input parameters: numrel,diff and eagerness,diff
+# target statistics: average relationships for both partners,
+# standard deviation  relationships for both partners, and
+# slope of the fit line between size of sub-trees and the number of sub-trees [target stat from a phylogeny]
+
+
+# Ex.3 phylogentic statistic: slope of the fit line between size of sub-trees and the number of sub-trees
+# input parameters: numrel,diff and eagerness,diff
+# target statistics: average relationships for both partners,
+# standard deviation  relationships for both partners, and
+# slope of the fit line between size of sub-trees and the number of sub-trees [target stat from a phylogeny]
+
+
+
