fb497b018550a67db7d455c57fb955c52469dc91
wdelva <wimdelva@gmail.com>
2017-08-07 13:52:58 +0200

    update

--- R/concurrent.point.prevalence.R
+++ R/concurrent.point.prevalence.R
@@ -7,7 +7,7 @@
 #' be retained, e.g. c(15, 30)
 #' @param timepoint Point in time at which the HIV prevalence should be calculated.
 #' @param hivstatus HIV status at the time of the survey. Options are 2, means all; 0
-#' means only HIV-negative, 1 means only HIV-positive. 
+#' means only HIV-negative, 1 means only HIV-positive.
 #' @return A dataframe with concurrent point prevalence estimate and surrounding confidence bounds,
 #' for the specified time point, age group and hiv status, overall, and stratified by gender
 #' @examples
@@ -17,24 +17,25 @@
 #' concurrent.pointprev.df
 #'
 #' @importFrom stats binom.test
-#' @importFrom dplyr as_tibble
+#' @import dplyr
+#' @importFrom tibble as_tibble
 #' @export
 
-concurr.pointprev.calculator <- function(datalist = datalist, 
+concurr.pointprev.calculator <- function(datalist = datalist,
                                          agegroup = c(15, 50),
                                          timepoint,
                                          hivstatus = 2){
   newtimepoint = timepoint - 0.5
   # We only take the data of people who were alive at timepoint
-  DTalive.infected <- alive.infected(datalist = datalist, timepoint = newtimepoint, site = "All") 
+  DTalive.infected <- alive.infected(datalist = datalist, timepoint = newtimepoint, site = "All")
   agemix <- agemix.df.maker(datalist = datalist)
-  
+
   # for males
   DTalive.infected.agegroup.men <- subset(DTalive.infected,
-                                          TOB <= newtimepoint - agegroup[1] & 
-                                            TOB > newtimepoint - agegroup[2] & 
+                                          TOB <= newtimepoint - agegroup[1] &
+                                            TOB > newtimepoint - agegroup[2] &
                                             Gender == 0)
-  
+
   degree.df <- degree.df.maker(dataframe.df = agemix,
                                agegroup = agegroup,
                                hivstatus = hivstatus,
@@ -42,33 +43,33 @@ concurr.pointprev.calculator <- function(datalist = datalist,
                                window.width = 0,
                                gender.degree = "male",
                                only.new = FALSE)
-  
+
   number.males.with.cps <- sum(degree.df$Degree > 1)
   popsize.males <- nrow(DTalive.infected.agegroup.men)
-  
+
   if(!is.na(number.males.with.cps)){
    concurr.pointprev.males <- number.males.with.cps / popsize.males
     pointprevalence.95.ll <- binom.test(x = number.males.with.cps, n = popsize.males)$conf.int[1]
     pointprevalence.95.ul <- binom.test(x = number.males.with.cps, n = popsize.males)$conf.int[2]
-  
-    concurr.pointprev.males.df <- cbind.data.frame(Gender = "male", 
-                                                   concurr.pointprev = concurr.pointprev.males, 
-                                                   pointprevalence.95.ll = pointprevalence.95.ll, 
+
+    concurr.pointprev.males.df <- cbind.data.frame(Gender = "male",
+                                                   concurr.pointprev = concurr.pointprev.males,
+                                                   pointprevalence.95.ll = pointprevalence.95.ll,
                                                    pointprevalence.95.ul = pointprevalence.95.ul)
   } else{
-    
-    concurr.pointprev.males.df <- cbind.data.frame(Gender = "male", 
-                                                   concurr.pointprev = NA, 
-                                                   pointprevalence.95.ll = NA, 
+
+    concurr.pointprev.males.df <- cbind.data.frame(Gender = "male",
+                                                   concurr.pointprev = NA,
+                                                   pointprevalence.95.ll = NA,
                                                    pointprevalence.95.ul = NA)
   }
-  
+
   # for females
   DTalive.infected.agegroup.female <- subset(DTalive.infected,
-                                             TOB <= newtimepoint - agegroup[1] & 
+                                             TOB <= newtimepoint - agegroup[1] &
                                                TOB > newtimepoint - agegroup[2] &
                                                Gender == 1)
-  
+
   degree.df <- degree.df.maker(dataframe.df = agemix,
                                agegroup = agegroup,
                                hivstatus = hivstatus,
@@ -76,46 +77,46 @@ concurr.pointprev.calculator <- function(datalist = datalist,
                                window.width = 0,
                                gender.degree = "female",
                                only.new = FALSE)
-  
+
   number.females.with.cps <- sum(degree.df$Degree > 1)
   popsize.females <- nrow(DTalive.infected.agegroup.female)
-  
+
   if(!is.na(number.females.with.cps)){
     concurr.pointprev.females <- number.females.with.cps / popsize.females
     pointprevalence.95.ll <- binom.test(x = number.females.with.cps, n = popsize.females)$conf.int[1]
     pointprevalence.95.ul <- binom.test(x = number.females.with.cps, n = popsize.females)$conf.int[2]
-  
+
     concurr.pointprev.females.df <- cbind.data.frame(Gender = "female",
-                                                     concurr.pointprev = concurr.pointprev.females, 
-                                                     pointprevalence.95.ll = pointprevalence.95.ll, 
+                                                     concurr.pointprev = concurr.pointprev.females,
+                                                     pointprevalence.95.ll = pointprevalence.95.ll,
                                                      pointprevalence.95.ul = pointprevalence.95.ul)
   } else{
-    
+
     concurr.pointprev.females.df <- cbind.data.frame(Gender = "female",
-                                                     concurr.pointprev = NA, 
-                                                     pointprevalence.95.ll = NA, 
+                                                     concurr.pointprev = NA,
+                                                     pointprevalence.95.ll = NA,
                                                      pointprevalence.95.ul = NA)
   }
-    
+
   # for all
 
   number.people.with.cps <- sum(number.males.with.cps, number.females.with.cps)
   popsize <- sum(popsize.males, popsize.females)
-  
+
   if(!is.na(number.people.with.cps)){
     concurr.pointprev <- number.people.with.cps / popsize
     pointprevalence.95.ll <- binom.test(x = number.people.with.cps, n = popsize)$conf.int[1]
     pointprevalence.95.ul <- binom.test(x = number.people.with.cps, n = popsize)$conf.int[2]
 
     concurr.pointprev.all.df <- cbind.data.frame(Gender = "all",
-                                                 concurr.pointprev = concurr.pointprev, 
-                                                 pointprevalence.95.ll = pointprevalence.95.ll, 
+                                                 concurr.pointprev = concurr.pointprev,
+                                                 pointprevalence.95.ll = pointprevalence.95.ll,
                                                  pointprevalence.95.ul = pointprevalence.95.ul)
   } else{
-    
+
     concurr.pointprev.all.df <- cbind.data.frame(Gender = "all",
-                                                 concurr.pointprev = NA, 
-                                                 pointprevalence.95.ll = NA, 
+                                                 concurr.pointprev = NA,
+                                                 pointprevalence.95.ll = NA,
                                                  pointprevalence.95.ul = NA)
   }
   concurr.pointprev.df <- rbind.data.frame(concurr.pointprev.males.df, concurr.pointprev.females.df,concurr.pointprev.all.df)
