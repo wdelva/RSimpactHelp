93581bca0da3dd4dd02e39062312b0f6a73b1faa
Trust Chibawara <trust@aims.ac.za>
2017-09-30 23:26:13 +0200

    Removed facility assignment improved the computation of the statistics

--- R/age.group.time.window.R
+++ R/age.group.time.window.R
@@ -21,52 +21,28 @@ age.group.time.window <- function(datalist = datalist, agegroup = c(15, 30),
                                   timewindow = c(15, 30), site="All"){
 
   DT <- datalist$ptable
-  DT$pfacility <- "NA"
-  pf.index <- which(colnames(DT)=="pfacility")
 
   if(site=="All"){
     DTexists.timewindow <- DT
   }else{
-    facilities.df <- datalist$ftable
-    colnames(facilities.df) <- c("facility.xy","XCoord","YCoord")
-
-    for(i in nrow(DT)){
-      fc.id <- which.min(sqrt((DT[i,XCoord] - facilities.df$XCoord)^2 +
-                                (DT[i,YCoord] - facilities.df$YCoord)^2))
-      DT[i, pf.index] <- facilities.df[fc.id, facility.xy]
-    }
-
     DTexists.timewindow <- subset(DT, pfacility==site)
   }
 
   #Convert lower age of interest into time
   DTexists.timewindow <- DTexists.timewindow %>%
-    dplyr::mutate(LowerTimeAgeGroup = TOB + agegroup[1])
-
-  DTexists.timewindow$LowerTimeWindow <- timewindow[1]
-
-  DTexists.timewindow <- DTexists.timewindow %>%
-    dplyr::mutate(exposure.start = pmax(LowerTimeAgeGroup, LowerTimeWindow))
-
-  #Convert upper age of interest into time
-  DTexists.timewindow <- DTexists.timewindow %>%
-    dplyr::mutate(UpperTimeAgeGroup = TOB + agegroup[2])
-
-  DTexists.timewindow$UpperTimeWindow <- timewindow[2]
-
-  DTexists.timewindow <- DTexists.timewindow %>%
-    dplyr::mutate(exposure.end = pmin(TOD, pmin(UpperTimeAgeGroup, UpperTimeWindow)))
-
-  #Exposure time (Everyone with exposure time greater than 0)
-  DTexists.timewindow <- DTexists.timewindow %>%
-    dplyr::mutate(exposure.time = exposure.end - exposure.start)
-
-  DTexists.timewindow <- DTexists.timewindow %>%
-    dplyr::mutate(real.exposure.time = exposure.time > 0)
-
-  #remove those that did not have exposure (died before timewindow or outside min age group)
-  DTexists.timewindow <- subset(DTexists.timewindow, real.exposure.time == TRUE )
-
+    dplyr::mutate(LowerTimeAgeGroup = TOB + agegroup[1],
+                  LowerTimeWindow = timewindow[1],
+                  exposure.start = pmax(LowerTimeAgeGroup, LowerTimeWindow),
+                  UpperTimeAgeGroup = TOB + agegroup[2], #Convert upper age of interest into time
+                  UpperTimeWindow = timewindow[2],
+                  exposure.end = pmin(TOD, pmin(UpperTimeAgeGroup, UpperTimeWindow)),
+                  exposure.time = exposure.end - exposure.start,
+                  #Exposure time (Everyone with exposure time greater than 0)
+                  real.exposure.time = exposure.time > 0
+                  ) %>%
+    subset(real.exposure.time == TRUE) %>%
+    #remove those that did not have exposure (died before timewindow or outside min age group)
+    as.data.frame
 
   return(DTexists.timewindow)
 }
