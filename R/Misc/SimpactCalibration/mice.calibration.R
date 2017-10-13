6e2d3db8e04dbb49affeb7f30b43e413294465c0
Trust Chibawara <trust@aims.ac.za>
2017-10-13 11:11:37 +0200

    Set up an example of mice calibration

--- R/Misc/SimpactCalibration/mice.calibration.R
+++ R/Misc/SimpactCalibration/mice.calibration.R
@@ -1,25 +1,50 @@
 #loading packages
-pacman::p_load(dplyr, mice)
+pacman::p_load(RSimpactCyan, RSimpactHelper, data.table, magrittr, dplyr,
+               ggplot2, mice, tidyr, lhs)
 
-dirname <- "~/Documents/GIT_Projects/SimulationParameterSetup"
+dirname <- "~/Documents/GIT_Projects/RSimpactHelp"
 
-comp <- "lin" #lin #mac #chpc #gent
 #dirname <- "/mnt/lustre/users/tchibawara/MaxART/data"
 #dirname <- "/user/data/gent/vsc400/vsc40070/simpact-test/data"
 
 #simpact simulation traing dataset
-training.df <- "1rep.mean.adj.sse.TrainingSet.csv"
+training.df <- "SummaryOutPut-df-iSNweKFVoZ.csv"
 
-#Read the saved training set
+#Read the saved data from the Simpact simulation
 complete.df.wna <- data.frame(read.csv(file = paste0(dirname,"/",training.df), header = TRUE))
+
+#remove all the NA and Nan values from the resulting simulation
+complete.df.wna <- subset(complete.df.wna, complete.cases(complete.df.wna))
+
+#Just to get the varied parameter names
+inPUT.df.complete <- source("R/Misc/SimpactWrapper/simpact.parameters.R")$value
+input.parameter.names <- names(dplyr::select(inPUT.df.complete, contains(".")))
+input.parameter.names <- input.parameter.names[2:length(input.parameter.names)]
+
+#get the target values and their names
+source("R/Misc/SimpactWrapper/summary.statistics.creator.R")
+
+#selection data for calibration
+filter.names <- c(target.variables, input.parameter.names)
+
+### target parameters with NA in simpact varied parameters
+target.parameter.na <- c(target.values, rep(NA, length(input.parameter.names)))
+
+#get only the df with the Target values and Simpact waried parameters
+complete.df.wna <- complete.df.wna[ , (names(complete.df.wna) %in% filter.names)]
+
+########      Now create the df for mice imputation #########################
+complete.df.wna <- rbind(complete.df.wna, target.parameter.na)
+
 
 mice.calibration <- function(df.wna, method.id = 1, out.sets = 5, iter.num = 5, seed.id = 2525){
 
   #selected methods used
   cal.list <- c("pmm", "norm", "norm.boot")
 
-  mice.imputted.df <- mice(df.wna, m=out.sets, maxit = iter.num,
-                           method = cal.list[method.id], seed = seed.id)
+  #calibration
+  mice.imputted.df <- mice(df.wna, m=out.sets, maxit = iter.num, method = cal.list[method.id],
+                           seed = seed.id)
 
   #collect all the imputed sets
   mice.out.imputted.df <- tail(mice::complete(mice.imputted.df,1),1)
@@ -30,16 +55,10 @@ mice.calibration <- function(df.wna, method.id = 1, out.sets = 5, iter.num = 5, 
   }
 
   #remove all the rows with out of range imputed files
-  mice.out.imputted.df <- subset(mice.out.imputted.df, person.agegap.man.dist.normal.mu >0)
-  mice.out.imputted.df <- subset(mice.out.imputted.df, person.agegap.woman.dist.normal.mu >0)
-  mice.out.imputted.df <- subset(mice.out.imputted.df, person.art.accept.threshold.dist.fixed.value < 0.9)
   mice.out.imputted.df <- subset(mice.out.imputted.df, person.agegap.man.dist.normal.sigma > 0)
-  mice.out.imputted.df <- subset(mice.out.imputted.df, hivtransmission.param.f1 >0)
+  mice.out.imputted.df <- subset(mice.out.imputted.df, hivtransmission.param.f1 > 0)
 
   mice.out.imputted.df$method <- cal.list[method.id]
-
-  write.csv(mice.out.imputted.df, file = paste0(dirname,"/mice.",cal.list[method.id],".",training.df),
-            row.names = FALSE)
 
   return(mice.out.imputted.df)
 
@@ -47,6 +66,10 @@ mice.calibration <- function(df.wna, method.id = 1, out.sets = 5, iter.num = 5, 
 
 #check time
 mice.startt <- as.numeric(proc.time()[3])
-mice.calibration <- mice.calibration(complete.df.wna, method.id = 1)
+mice.calibration <- mice.calibration(complete.df.wna, method.id = 1, out.sets = 10)
+save.string <- paste0(sample(c(LETTERS,letters), 5), collapse = "")
+write.csv(mice.calibration, file = paste0(dirname,"/mice.",cal.list[method.id],".",save.string,
+                                          ".",training.df), row.names = FALSE)
+
 mice.end.pmm <- as.numeric(proc.time()[3]) - mice.startt
 
