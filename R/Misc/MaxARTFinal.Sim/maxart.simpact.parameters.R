e4255d9dc566123ff19aba7346d47413c9d0810f
Trust Chibawara <trust@aims.ac.za>
2017-10-19 13:26:55 +0200

    make simpact parameter source a function

--- R/Misc/MaxARTFinal.Sim/maxart.simpact.parameters.R
+++ R/Misc/MaxARTFinal.Sim/maxart.simpact.parameters.R
@@ -2,43 +2,30 @@
 #Idea is to give the simpact parameters and their ranges
 # to be calibrated during the simpact run
 
-init.design.points <- 40000 #set the initial design points
+source.simpact.parameters <- function(init.design.points = 3, resample = 1){
 
-#choose if select ids or just a range
-sel.list <- "minmax" #min.max
+  ###### Generate the input parameters for the simulation #################################
+  simpact.input.par <- simpact.config.inputs(design.points = init.design.points, resample.count = 1,
+                                             conception.alpha_base = c(-5, -0.1),
+                                             person.art.accept.threshold.dist.fixed.value = c(0.4, 1),
+                                             person.eagerness.man.dist.gamma.a = c(0.3, 1.5),
+                                             person.eagerness.man.dist.gamma.b = c(10, 60),
+                                             person.eagerness.woman.dist.gamma.a = c(0.3, 1.5),
+                                             formation.hazard.agegapry.eagerness_diff =  c(-0.1, 0),
+                                             person.eagerness.woman.dist.gamma.b = c(10, 60),
+                                             formation.hazard.agegapry.numrel_man = c(-2, -0.1),
+                                             formation.hazard.agegapry.numrel_woman = c(-2, -0.1),
+                                             formation.hazard.agegapry.gap_factor_man_exp = c(-2, -0.01),
+                                             formation.hazard.agegapry.gap_factor_woman_exp = c(-2, -0.01),
+                                             person.agegap.man.dist.normal.mu = c(1, 5),
+                                             person.agegap.woman.dist.normal.mu = c(1, 5),
+                                             person.agegap.man.dist.normal.sigma = c(0.5, 3.5),
+                                             person.agegap.woman.dist.normal.sigma = c(0.5, 3.5),
+                                             hivtransmission.param.f1 = c(log(2), log(3.5))
+  )
 
-if(sel.list == "list"){
-  #set the list of ids that you want to simulate
-  sel.id.list <- c(7578, 8302, 11728, 30400)
-  }else{
-    #Select a chunk to process
-    min.chunk <- 12
-    max.chunk <- 12
+  return(simpact.input.par)
+
 }
-
-#set how many time the single row will be repeated
-sim_repeat <- 2
-#number of cores per node
-ncluster.use <- 4
-
-###### Generate the input parameters for the simulation #################################
-simpact.config.inputs(design.points = init.design.points, resample.count = 1,
-                       conception.alpha_base = c(-5, -0.1),
-                       person.art.accept.threshold.dist.fixed.value = c(0.4, 1),
-                       person.eagerness.man.dist.gamma.a = c(0.3, 1.5),
-                       person.eagerness.man.dist.gamma.b = c(10, 60),
-                       person.eagerness.woman.dist.gamma.a = c(0.3, 1.5),
-                       formation.hazard.agegapry.eagerness_diff =  c(-0.1, 0),
-                       person.eagerness.woman.dist.gamma.b = c(10, 60),
-                       formation.hazard.agegapry.numrel_man = c(-2, -0.1),
-                       formation.hazard.agegapry.numrel_woman = c(-2, -0.1),
-                       formation.hazard.agegapry.gap_factor_man_exp = c(-2, -0.01),
-                       formation.hazard.agegapry.gap_factor_woman_exp = c(-2, -0.01),
-                       person.agegap.man.dist.normal.mu = c(1, 5),
-                       person.agegap.woman.dist.normal.mu = c(1, 5),
-                       person.agegap.man.dist.normal.sigma = c(0.5, 3.5),
-                       person.agegap.woman.dist.normal.sigma = c(0.5, 3.5),
-                       hivtransmission.param.f1 = c(log(2), log(3.5))
-)
 ############### Argument sample above ##################################################
 
--- R/Misc/SimpactWrapper/simpact.parameters.R
+++ R/Misc/SimpactWrapper/simpact.parameters.R
@@ -2,32 +2,20 @@
 #Idea is to give the simpact parameters and their ranges
 # to be calibrated during the simpact run
 
-init.design.points <- 10 #set the initial design points
-
-#choose if select ids or just a range
-sel.list <- "min.max" #min.max
-
-if(sel.list == "list"){
-  #set the list of ids that you want to simulate
-  sel.id.list <- c(75, 83)
-  }else{
-    #Select a chunk to process
-    min.chunk <- 1
-    max.chunk <- 10
-}
-
-#set how many time the single row will be repeated
-sim_repeat <- 1
-#number of cores per node
-ncluster.use <- 4
+source.simpact.parameters <- function(init.design.points = 3, resample = 1){
 
 ###### Generate the input parameters for the simulation #################################
-simpact.config.inputs(design.points = init.design.points, resample.count = 1,
-                       conception.alpha_base = c(-5, -0.1),
-                       person.art.accept.threshold.dist.fixed.value = c(0.4, 1),
-                       person.agegap.man.dist.normal.sigma = c(0.5, 3.5),
-                       person.agegap.woman.dist.normal.sigma = c(0.5, 3.5),
-                       hivtransmission.param.f1 = c(log(2), log(3.5))
-)
+  simpact.input.par <- simpact.config.inputs(design.points = init.design.points, resample.count = 1,
+                                             conception.alpha_base = c(-5, -0.1),
+                                             person.art.accept.threshold.dist.fixed.value = c(0.4, 1),
+                                             person.agegap.man.dist.normal.sigma = c(0.5, 3.5),
+                                             person.agegap.woman.dist.normal.sigma = c(0.5, 3.5),
+                                             hivtransmission.param.f1 = c(log(2), log(3.5))
+  )
+  return(simpact.input.par)
+
+}
+
+
 ############### Argument sample above ##################################################
 
