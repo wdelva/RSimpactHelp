4ce3d4e213416fa2c94752c46452456667d81a10
Roxanne Beauclair <roxannebeauclair@Roxannes-MacBook-Pro-2.local>
2017-12-13 12:56:20 +0200

    Updated documentation for alive.infected()

--- R/alive.infected.R
+++ R/alive.infected.R
@@ -1,17 +1,25 @@
-#' Subset the datalist ptable to include only people who were alive at a point
-#'  in time and add their HIV status at that point in time.
+#' Subset people alive at a point in time and adds their HIV status.
+#' 
+#' Produces a data frame that contains persons from the simulation that were
+#' alive at a specified point in time. The new data frame also contains a
+#' variable that indicates whether or not the person was infected with HIV at
+#' that point in time.
 #'
-#' @param datalist The datalist that is produced by \code{\link{readthedata}}
-#' @param timepoint Point in time at which the subset should be created and HIV status
-#' should be evaluated.
-#' @param site Select only the particular site from the study, if all ignore site/use all sites.
-#' @return a data.table that only includes people who were alive at the timepoint and that
-#' records their HIV status.
+#' @param datalist The list object that is produced by \code{\link{readthedata}}
+#' @param timepoint Point in time at which the subset should be created and HIV 
+#'   status should be evaluated.
+#' @param site Users can specify a particular site they are interested in. The 
+#'   default is "All", which means that persons from all sites should be 
+#'   included.
+#' @return A data frame that contains a newly generated variable called 
+#'   "Infected" that records the HIV status of persons at the time point
+#'   indicated.
 #' @examples
-#' data(datalist)
-#' alive.at.twenty.dt <- alive.infected(datalist = datalist, timepoint = 20,
-#' site = "All")
-#'
+#' cfg <- list()
+#' modeloutput <- RSimpactCyan::simpact.run(configParams = cfg, destDir = "temp")
+#' dl <- readthedata(modeloutput)
+#' alive.at.twenty.df <- alive.infected(datalist = dl, timepoint = 20)
+#' 
 #' @importFrom magrittr %>%
 #' @import dplyr
 #' @export
