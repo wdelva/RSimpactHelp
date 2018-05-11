a544df5bd7c4854d6798dd8ce2e79047da4dce9c
David Niyukuri <niyukuri@aims.ac.za>
2018-05-11 23:32:00 +0200

    Function to automate ID's selection based on age groups and gender ratio of individuals in a transmission network

---
+++ R/select.indiv.seq.age.diff.fun.R
@@ -0,0 +1,134 @@
+
+#' Subset sequences based on gender ratio and age groups of individuals
+#'
+#' @param simpact.trans.net Transmission networks computed by \code{\link{transmission.network.builder()}}
+#' @param limitTransmEvents Choose transmission network with at least this  amount of individuals
+#' @param perc.men Precentage of men to consider when selecting the sequences
+#' @param age.men Age group for men
+#' @param age.women Age group for women
+#' @return A list with a vector of ID's of selected sequences, number of men, and women for the selcted sequences and the ratio between men and women among the selcted sequences because in some settings we may have less or more female or male individuals
+#' @importFrom dplyr filter
+#' @export
+#' 
+select.indiv.seq.age.diff.fun <- function(simpact.trans.net = simpact.trans.net,
+                                          limitTransmEvents = 3,
+                                          perc.men = 30, 
+                                          age.men=c(15,60),
+                                          age.women=c(15,40)){
+  
+  seeds.id <- length(simpact.trans.net)
+  
+  new.transm.tab <- vector("list", length(seeds.id))
+  
+  for(i in 1:seeds.id){
+    
+    transm.age.i <- as.data.frame(simpact.trans.net[[i]])
+    
+    age.i <- transm.age.i$SampTime - transm.age.i$TOBRec
+    
+    transm.age.i <- cbind(transm.age.i, age.i)
+    
+    new.transm.tab[[i]] <- transm.age.i
+    
+  }
+  
+  # Selceted networks
+  
+  IDs.transm <- vector()
+  
+  TransmEventsCountVector <- vector()
+  
+  for(k in 1:seeds.id){
+    trans.net.i.check <- as.data.frame(new.transm.tab[[k]])
+    
+    if(nrow(trans.net.i.check)>=limitTransmEvents){
+      
+      TransmEventsCountVector <- c(TransmEventsCountVector, nrow(trans.net.i.check))
+      
+      IDs.transm <- c(IDs.transm, k)
+    }
+  }
+  
+  ## Binding transmission transmission networks ##
+  
+  for (q in 1:length(IDs.transm)){
+    
+    p <- IDs.transm[q]
+    
+    if(q==1){
+      trans.sum <- new.transm.tab[[p]]
+      rename.id <- paste0(p,".",trans.sum$id,".C")
+      trans.sum$id <- rename.id
+      trans.sum.rename.id <- trans.sum
+    }
+    else{
+      
+      read.trans.sum <- new.transm.tab[[p]]
+      rename.id.read <- paste0(p,".",read.trans.sum$id,".C")
+      read.trans.sum$id <- rename.id.read
+      trans.sum.rename.id <- rbind(trans.sum.rename.id, read.trans.sum)
+    }
+  }
+  
+  trans.sum.men <- dplyr::filter(trans.sum.rename.id, trans.sum.rename.id$GenderRec=="0")
+  
+  trans.sum.women <- dplyr::filter(trans.sum.rename.id, trans.sum.rename.id$GenderRec=="1")  
+  
+  trans.sum.age.limit.men <- dplyr::filter(trans.sum.men, trans.sum.men$age.i>=age.men[1], trans.sum.men$age.i<=age.men[2])
+  
+  trans.sum.age.limit.women <- dplyr::filter(trans.sum.women, trans.sum.women$age.i>=age.women[1], trans.sum.women$age.i<=age.women[2])
+  
+  
+  
+  
+  perc.100 <- nrow(trans.sum.age.limit.men) + nrow(trans.sum.age.limit.women) #
+  
+  nrow.men <- nrow(trans.sum.age.limit.men) # number of available men
+  
+  nrow.women <- nrow(trans.sum.age.limit.women) # number of available women
+  
+  men <- round((perc.100*perc.men)/100) # number of wanted men
+  
+  women <- perc.100 - men # number of wanted women
+  
+  diff.men <- nrow.men - men # difference between number of available and wanted men
+  
+  diff.women <- nrow.women - women # difference between number of available and wanted women
+  
+  ratio <- men/women
+  
+  if(diff.men >0 & diff.women >0){
+    
+    samp.men <- sample(trans.sum.men$id, men)
+    samp.women <- sample(trans.sum.women$id, women)
+    
+  } else if(diff.men <0 & diff.women >0){
+    
+    samp.men <- sample(trans.sum.men$id, nrow.men)
+    samp.women <- sample(trans.sum.women$id, women)
+    
+  } else if(diff.men >0 & diff.women <0){
+    
+    samp.men <- sample(trans.sum.men$id, men)
+    samp.women <- sample(trans.sum.women$id, nrow.women) 
+    
+  } else {   #if(diff.men <0 & diff.women <0){
+    samp.men <- sample(trans.sum.men$id, nrow.men)
+    samp.women <- sample(trans.sum.women$id, nrow.women) 
+    
+  }
+  
+  outputvector <- c(samp.men, samp.women)
+  
+  outputvec.stat <- list()
+  
+  outputvec.stat$outputvector <- outputvector
+  outputvec.stat$men <- men
+  outputvec.stat$women <- women
+  outputvec.stat$ratio <- ratio
+  
+  return(outputvec.stat)
+  
+}
+
+
