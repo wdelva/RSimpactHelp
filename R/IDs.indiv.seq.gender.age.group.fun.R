eafa5362c731cca65ee5dcbf82975cf8c1924a16
David Niyukuri <niyukuri@aims.ac.za>
2018-05-16 13:27:19 +0200

    condition when we don't have any transmission network with at least limitTransmEvents individuals

--- R/IDs.indiv.seq.gender.age.group.fun.R
+++ R/IDs.indiv.seq.gender.age.group.fun.R
@@ -56,88 +56,104 @@ IDs.indiv.seq.gender.age.group.fun <- function(simpact.trans.net = simpact.trans
 
   ## Binding together all selected transmission transmission networks ##
 
-  for (q in 1:length(IDs.transm)){
+  if(length(IDs.transm)>=1){
 
-    p <- IDs.transm[q]
+    for (q in 1:length(IDs.transm)){
 
-    if(q==1){
-      trans.sum <- new.transm.tab[[p]]
-      rename.id <- paste0(p,".",trans.sum$id,".C")
-      trans.sum$id <- rename.id
-      trans.sum.rename.id <- trans.sum
+      p <- IDs.transm[q]
+
+      if(q==1){
+        trans.sum <- new.transm.tab[[p]]
+        rename.id <- paste0(p,".",trans.sum$id,".C")
+        trans.sum$id <- rename.id
+        trans.sum.rename.id <- trans.sum
+      }
+      else{
+
+        read.trans.sum <- new.transm.tab[[p]]
+        rename.id.read <- paste0(p,".",read.trans.sum$id,".C")
+        read.trans.sum$id <- rename.id.read
+        trans.sum.rename.id <- rbind(trans.sum.rename.id, read.trans.sum)
+      }
     }
-    else{
 
-      read.trans.sum <- new.transm.tab[[p]]
-      rename.id.read <- paste0(p,".",read.trans.sum$id,".C")
-      read.trans.sum$id <- rename.id.read
-      trans.sum.rename.id <- rbind(trans.sum.rename.id, read.trans.sum)
+
+    trans.sum.men <- dplyr::filter(trans.sum.rename.id, trans.sum.rename.id$GenderRec=="0")
+
+    trans.sum.women <- dplyr::filter(trans.sum.rename.id, trans.sum.rename.id$GenderRec=="1")
+
+    trans.sum.age.limit.men <- dplyr::filter(trans.sum.men, trans.sum.men$age.i>=age.men[1], trans.sum.men$age.i<=age.men[2])
+
+    trans.sum.age.limit.women <- dplyr::filter(trans.sum.women, trans.sum.women$age.i>=age.women[1], trans.sum.women$age.i<=age.women[2])
+
+    perc.100 <- nrow(trans.sum.age.limit.men) + nrow(trans.sum.age.limit.women) # total number of individuals with age limit
+
+    perc.seq.coverage <- round(perc.100*seq.cov/100) # total number of wanted individuals at seq.cov sequence coverage
+
+    nrow.men <- nrow(trans.sum.age.limit.men) # number of available men
+
+    nrow.women <- nrow(trans.sum.age.limit.women) # number of available women
+
+    men.seq.coverage <- round((perc.seq.coverage*perc.men)/100) # number of wanted men
+
+    women.seq.coverage <- perc.seq.coverage - men.seq.coverage # numbe rof wanted women
+
+    diff.men <- nrow.men - men.seq.coverage # difference between number of available and wanted men
+
+    diff.women <- nrow.women - women.seq.coverage # difference between number of available and wanted women
+
+    ratio.emp <- perc.men/(100-perc.men)
+
+    if(diff.men >0 & diff.women >0){ # perfect case
+
+      samp.men <- sample(trans.sum.age.limit.men$id, men.seq.coverage)
+      samp.women <- sample(trans.sum.age.limit.women$id, women.seq.coverage)
+      ratio.seq <- men.seq.coverage/women.seq.coverage
+
+    } else if(diff.men <0 & diff.women >0){ # we have less men than what we need
+
+      samp.men <- sample(trans.sum.age.limit.men$id, nrow.men) # take all we have
+      samp.women <- sample(trans.sum.age.limit.women$id, women.seq.coverage)
+      ratio.seq <- nrow.men/women.seq.coverage
+
+    } else if(diff.men >0 & diff.women <0){ # we have less women than what we need
+
+      samp.men <- sample(trans.sum.age.limit.men$id, men.seq.coverage)
+      samp.women <- sample(trans.sum.age.limit.women$id, nrow.women) # take all we have
+      ratio.seq <- men.seq.coverage/nrow.women
+
+    } else {   #if(diff.men <0 & diff.women <0){ # we have less men and women than what we need
+      samp.men <- sample(trans.sum.age.limit.men$id, nrow.men) # take all we have
+      samp.women <- sample(trans.sum.age.limit.women$id, nrow.women) # take all we have
+      ratio.seq <- nrow.men/nrow.women
+
     }
+
+
+    outputvector <- c(samp.men, samp.women)
+
+    outputvec.stat <- list()
+
+    outputvec.stat$outputvector <- outputvector
+    outputvec.stat$men <- men.seq.coverage
+    outputvec.stat$women <- women.seq.coverage
+    outputvec.stat$ratio.seq <- ratio.seq
+    outputvec.stat$ratio.emp <- ratio.emp
+
+    return(outputvec.stat)
+
+  }else{
+
+
+    outputvec.stat <- list()
+
+    outputvec.stat$outputvector <- outputvector <- NA
+    outputvec.stat$men <- men.seq.coverage <- NA
+    outputvec.stat$women <- women.seq.coverage <- NA
+    outputvec.stat$ratio.seq <- ratio.seq <- NA
+    outputvec.stat$ratio.emp <- ratio.emp <- NA
+
+    return(outputvec.stat)
   }
-
-
-  trans.sum.men <- dplyr::filter(trans.sum.rename.id, trans.sum.rename.id$GenderRec=="0")
-
-  trans.sum.women <- dplyr::filter(trans.sum.rename.id, trans.sum.rename.id$GenderRec=="1")
-
-  trans.sum.age.limit.men <- dplyr::filter(trans.sum.men, trans.sum.men$age.i>=age.men[1], trans.sum.men$age.i<=age.men[2])
-
-  trans.sum.age.limit.women <- dplyr::filter(trans.sum.women, trans.sum.women$age.i>=age.women[1], trans.sum.women$age.i<=age.women[2])
-
-  perc.100 <- nrow(trans.sum.age.limit.men) + nrow(trans.sum.age.limit.women) # total number of individuals with age limit
-
-  perc.seq.coverage <- round(perc.100*seq.cov/100) # total number of wanted individuals at seq.cov sequence coverage
-
-  nrow.men <- nrow(trans.sum.age.limit.men) # number of available men
-
-  nrow.women <- nrow(trans.sum.age.limit.women) # number of available women
-
-  men.seq.coverage <- round((perc.seq.coverage*perc.men)/100) # number of wanted men
-
-  women.seq.coverage <- perc.seq.coverage - men.seq.coverage # numbe rof wanted women
-
-  diff.men <- nrow.men - men.seq.coverage # difference between number of available and wanted men
-
-  diff.women <- nrow.women - women.seq.coverage # difference between number of available and wanted women
-
-  ratio.emp <- perc.men/(100-perc.men)
-
-  if(diff.men >0 & diff.women >0){ # perfect case
-
-    samp.men <- sample(trans.sum.age.limit.men$id, men.seq.coverage)
-    samp.women <- sample(trans.sum.age.limit.women$id, women.seq.coverage)
-    ratio.seq <- men.seq.coverage/women.seq.coverage
-
-  } else if(diff.men <0 & diff.women >0){ # we have less men than what we need
-
-    samp.men <- sample(trans.sum.age.limit.men$id, nrow.men) # take all we have
-    samp.women <- sample(trans.sum.age.limit.women$id, women.seq.coverage)
-    ratio.seq <- nrow.men/women.seq.coverage
-
-  } else if(diff.men >0 & diff.women <0){ # we have less women than what we need
-
-    samp.men <- sample(trans.sum.age.limit.men$id, men.seq.coverage)
-    samp.women <- sample(trans.sum.age.limit.women$id, nrow.women) # take all we have
-    ratio.seq <- men.seq.coverage/nrow.women
-
-  } else {   #if(diff.men <0 & diff.women <0){ # we have less men and women than what we need
-    samp.men <- sample(trans.sum.age.limit.men$id, nrow.men) # take all we have
-    samp.women <- sample(trans.sum.age.limit.women$id, nrow.women) # take all we have
-    ratio.seq <- nrow.men/nrow.women
-
-  }
-
-  outputvector <- c(samp.men, samp.women)
-
-  outputvec.stat <- list()
-
-  outputvec.stat$outputvector <- outputvector
-  outputvec.stat$men <- men.seq.coverage
-  outputvec.stat$women <- women.seq.coverage
-  outputvec.stat$ratio.seq <- ratio.seq
-  outputvec.stat$ratio.emp <- ratio.emp
-
-  return(outputvec.stat)
 
 }
-
