ee693d87c4b221d48dfe6a5e539a62e8079b052b
wdelva <Wim.Delva@UGent.be>
2018-06-22 12:56:03 +0200

    added function wave1.creator

--- R/wave1.creator.R
+++ R/wave1.creator.R
@@ -1,6 +1,17 @@
+#' Create a csv file with parameter combinations for the first wave of Simpact simulations
+#'
+#' A short description here...
+#'
+#' @param file.name Where the csv file will be stored, and its name
+#' @param n.experiments Number of parameter combinations to be created
+#' @param limits list of prior distributions, one per parameter
+#' @return a csv file, written to disk
+#' @importFrom randtoolbox sobol
+#' @export
+
 wave1.creator <- function(file.name = "/user/data/gent/vsc400/vsc40070/agemixing/scripts/wave1.csv",
                           n.experiments,
-                          limits){ # limits <- list(prior.x.1 = c(0,1), prior.x.2 = c(2,4), prior.x.3 = c(-1,1))
+                          limits){ # e.g.: list(prior.x.1 = c(0,1), prior.x.2 = c(2,4), prior.x.3 = c(-1,1))
   lls <- as.numeric(as.data.frame(limits)[1, ])
   uls <- as.numeric(as.data.frame(limits)[2, ])
   range.width <- uls - lls
